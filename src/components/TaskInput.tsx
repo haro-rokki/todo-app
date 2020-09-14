dab4929c4374987acf49b09b5bcd1cd96db3fb2f
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-14 18:49:39 +0900

    タスクの追加

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,35 +1,54 @@
 import React, { useState } from 'react'
 import { Task } from './Types'
 import { Button, Input } from 'semantic-ui-react'
+import gql from 'graphql-tag'
+import { useMutation } from 'react-apollo'
 
 type Props = {
   setTasks: React.Dispatch<React.SetStateAction<Task[]>>
   tasks: Task[]
 }
 
+const ADD_TODO = gql`
+  mutation AddTodo($title: String!) {
+    addTodo(title: $title) {
+      id
+      title
+      done
+    }
+  }
+`
+
 const TaskInput: React.FC<Props> = ({ setTasks, tasks }) => {
   const [inputTitle, setInputTitle] = useState<string>('')
-  const [count, setCount] = useState<number>(tasks.length + 1)
+  const [addTodo, { data }] = useMutation<{ addTodo: Task }, { title: String }>(
+    ADD_TODO,
+    {
+      variables: { title: inputTitle },
+    },
+  )
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputTitle(e.target.value)
   }
 
-  const handleSubmit = () => {
-    setCount(count + 1)
+  const handleSubmit = async () => {
+    const res = await addTodo()
 
+    // TODO: エラーハンドリングちゃんとやる
+    const todoId = res.data ? res.data.addTodo.id : process.exit(1)
     const newTask: Task = {
-      id: count,
+      id: todoId,
       title: inputTitle,
       done: false,
     }
-
     setTasks([newTask, ...tasks])
     setInputTitle('')
   }
 
   return (
     <div>
+      {data && data.addTodo ? <p>Saved!</p> : null}
       <div className="inputForm">
         <div className="inner">
           <Input
