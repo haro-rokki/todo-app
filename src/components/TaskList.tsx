d7bf67bacc8373d0fe3741c2fcf86e0af3200ef3
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-09-14 19:17:02 +0900

    タスクの削除

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TaskItem from './TaskItem'
 import { Task } from './Types'
 import { List } from 'semantic-ui-react'
+import gql from 'graphql-tag'
+import { useMutation } from 'react-apollo'
 
 type Props = {
   tasks: Task[]
   setTasks: React.Dispatch<React.SetStateAction<Task[]>>
 }
 
+const DELETE_TODO = gql`
+  mutation DeleteTodo($id: ID!) {
+    deleteTodo(id: $id) {
+      id
+      title
+      done
+    }
+  }
+`
+
 const TaskList: React.FC<Props> = ({ tasks, setTasks }) => {
+  const [todoId, setTodoId] = useState<string>('')
+  const [deleteTodo, { data }] = useMutation<
+    { deleteTodo: Task },
+    { id: String }
+  >(DELETE_TODO, {
+    variables: { id: todoId },
+  })
+
   const handleDone = (task: Task) => {
     setTasks((prev) =>
       prev.map((t) => (t.id === task.id ? { ...task, done: !task.done } : t)),
     )
   }
 
-  const handleDelete = (task: Task) => {
+  const handleDelete = async (task: Task) => {
+    await setTodoId(task.id)
+    await deleteTodo()
+    setTodoId('')
     setTasks((prev) => prev.filter((t) => t.id !== task.id))
   }
 
   return (
     <div className="inner">
+      {data && data.deleteTodo ? <p>Deleted!</p> : null}
       {tasks.length <= 0 ? (
         '登録されたTODOはありません'
       ) : (
